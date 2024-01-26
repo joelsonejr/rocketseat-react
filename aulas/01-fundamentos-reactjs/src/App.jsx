3b816bec8cbe5438a158424fe0700c4cabd726d4
Joelson Junior <joelson.ejr@gmail.com>
2024-01-26 12:22:51 -0300

    Added "global.css" file; Performed the necessary changes, cause due the new file name.

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import "./global.css";
 
 export function App() {
   return (
     <>
+      <Header />
       <Post author="Joe" content="Esse é o meu Post" />
       <Post author="Zaki" content="Papai!" />
     </>
